449bd3fad318df8351f0516d49d976c3a11d343d
Vinay Dawani <vinay.dawani@perficient.com>
2022-02-07 15:14:37 -0500

    using better regex to scan for messages

--- src/service/events/messages.py
+++ src/service/events/messages.py
@@ -1,3 +1,6 @@
+import re
+
+
 def analyze_wordle_score(msg_payload: dict) -> dict:
     """Gets info from raw slack response
 
@@ -8,9 +11,11 @@ def analyze_wordle_score(msg_payload: dict) -> dict:
         dict: a dictionary of formatted data about user and the game
     """
     score_str = msg_payload["text"]
-    score_arr = score_str.split("\n")
-    game_info = score_arr[0]
-    game_res = score_arr[2:]
+    # score_arr = score_str.split("\n")
+    game_info = re.search("(Wordle) \d+ [123456X]\/6\*?", score_str).group()
+    game_res = re.search("((:\w+:(\\n)?)+)", score_str).group()
+
+    print(game_res, game_info)
 
     game_info = format_game_info(game_info, game_res)
     return game_info
@@ -29,9 +34,11 @@ def format_game_info(game_info: str, game_res_str: str) -> dict:
     info = game_info.split(" ")
     game_num = info[1]
     game_res = info[2][0]
+    if game_res == "X":
+        game_res = 0
     hard_mode = True if "*" in info[2] else False
     completed = was_game_completed(game_res_str)
-    shared = "\n".join(game_res_str)
+    shared = game_res_str
 
     return {
         "game_num": int(game_num),
@@ -43,7 +50,7 @@ def format_game_info(game_info: str, game_res_str: str) -> dict:
 
 
 def was_game_completed(game_res: str) -> bool:
-    """Checks if the game was completed nadn returns bool 
+    """Checks if the game was completed and returns bool
 
     Args:
         game_res (str): emoji part of the result string
@@ -51,6 +58,8 @@ def was_game_completed(game_res: str) -> bool:
     Returns:
         bool: if the game was completed
     """
+    game_res = game_res.split("\n")
+
     if len(game_res) <= 6:
         if (
             game_res[-1]
